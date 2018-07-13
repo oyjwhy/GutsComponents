89b514cd8ef89db39e2e73d2975b3f4e9302a5cb
oyjwhy <757551893@qq.com>
2018-07-13 21:43:07 +0800

    增加新闻列表部分

--- Guts_demo/src/components/NewsList.vue
+++ Guts_demo/src/components/NewsList.vue
@@ -1,14 +1,29 @@
 <template>
-  <div>
+  <div style="width:80%;margin-left:10%;">
     <!-- <a-button type="primary">Primary</a-button>
     <a-button>Default</a-button>
     <a-button type="dashed">Dashed</a-button>
     <a-button type="danger">Danger</a-button> -->
-    <a-card title="Card Title">
-      <a href="#" slot="extra">more</a>
-      <p>card content</p>
-      <p>card content</p>
-      <p>card content</p>
+    <a-card title="News Title">
+      <a-col :span="6"><img style="height:200px;" src='https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png' /></a-col>
+      <a-col :span="18">
+        
+      <a-row>
+        <a-col :span="8">日期</a-col>
+        <a-col :span="16"></a-col>
+      </a-row>
+
+      <a-row>
+        <a-col :span="8">撰稿人</a-col>
+        <a-col :span="16"></a-col>
+      </a-row>
+
+      <a-row>
+        <a-col :span="8">新闻摘要</a-col>
+        <a-col :span="16"></a-col>
+      </a-row>
+      </a-col>
+
     </a-card>
 
   </div>
@@ -21,6 +36,5 @@ export default {
 </script>
 
 <style>
-
 
 </style>
